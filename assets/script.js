9c0f58122a68b6174963f57feeacff3ea16b2f7b
Edluel <edluel_machado@hotmail.com>
2023-02-04 23:28:36 -0300

    bug-fix: hover always-on on mobile.

--- assets/script.js
+++ assets/script.js
@@ -8,8 +8,8 @@ async function showRandom() {
 
     id.innerHTML = data.slip.id;
     advice.textContent = data.slip.advice;
-}
 
+}
 showRandom();
 
 
@@ -25,8 +25,27 @@ function resizeAdvice() {
     var newHeight = height * (newWidth / width);
     advice.style.width = newWidth + "px";
     advice.style.height = newHeight + "px";
-  }
-}
 
+    //If the advice was resized then change the hover behavior
+    document.querySelector(".rgenerate-bt").addEventListener("touchstart", function() {
+        this.style.boxShadow = "0 0 20px var(--Neon-Green)";
+    });
+    document.querySelector(".rgenerate-bt").addEventListener("touchend", function() {
+        this.style.boxShadow = "none";
+    });
+  }
+    
+
+}
 resizeAdvice();
 
+
+
+/* /integer to user search by id and /search/string to search by word
+word results are a object with arrays and to acces each array item 
+use slips[index] instead of slip*/
+
+/* minha idéia para melhoria, adicionar barra de busca por id e por
+palavra, porém a busca por palávra deve invés de exibir o array
+de resultados deve implementar uma função de carrossel onde
+o usuario pode navegar pelos avices retornados pela busca */
