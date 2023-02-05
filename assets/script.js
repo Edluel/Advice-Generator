50c7aeded9b87644ec9f4959b7a94a9ef0be6968
Edluel <edluel_machado@hotmail.com>
2023-02-04 23:12:07 -0300

    making it responsive

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 async function showRandom() {
+
     const response = await fetch('https://api.adviceslip.com/advice');
     const data = await response.json();
 
@@ -10,3 +11,22 @@ async function showRandom() {
 }
 
 showRandom();
+
+
+
+function resizeAdvice() {
+  var advice = document.querySelector(".advice");
+  var width = advice.offsetWidth;
+  var height = advice.offsetHeight;
+  var maxWidth = window.innerWidth * 0.8;
+
+  if (width > maxWidth) {
+    var newWidth = maxWidth;
+    var newHeight = height * (newWidth / width);
+    advice.style.width = newWidth + "px";
+    advice.style.height = newHeight + "px";
+  }
+}
+
+resizeAdvice();
+
